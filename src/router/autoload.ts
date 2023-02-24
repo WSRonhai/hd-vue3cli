d5bb4ea0bf301b1b2a020315daa50e8c0a61ecf1
隆海 <122286911@qq.com>
2023-02-24 21:38:53 +0800

    自动注册路由和子路由

--- src/router/autoload.ts
+++ src/router/autoload.ts
@@ -3,57 +3,44 @@
  */
 import { RouteRecordRaw } from "vue-router";
 
-const layouts = import.meta.glob('../layouts/*.vue')
-const views = import.meta.glob('../views/**/*.vue')
-
+const layouts = import.meta.glob("../layouts/*.vue");
+const views = import.meta.glob("../views/**/*.vue");
 
 //获取主路由
 function getRoutes() {
-    const layoutRoutes = [] as RouteRecordRaw[]
-    Object.entries(layouts).forEach(([file, module]) => {
-        const route = getRoutesByModule(file, module)
-        console.log(111,route);
-        let child = getChildrenRoutes(route)
-        layoutRoutes.push(route)
-    })
-    return layoutRoutes
+  const layoutRoutes = [] as RouteRecordRaw[];
+  Object.entries(layouts).forEach(([file, module]) => {
+    const route = getRoutesByModule(file, module);
+    route.children = getChildrenRoutes(route);
+
+    layoutRoutes.push(route);
+  });
+  return layoutRoutes;
 }
+
 //获取子路由
-function getChildrenRoutes(layoutRoute: RouteRecordRaw): RouteRecordRaw[] {
-    const routes = [] as RouteRecordRaw[]
-
-    Object.entries(views).forEach(([file, module]) => {
-        if (file.includes(`../views/${layoutRoute.name as string}`)) {
-            
-            const route = getRoutesByModule(file, module)
-            console.log(222,file);
-            routes.push(route)
-        }
-    })
-    return routes
+function getChildrenRoutes(layoutRoute: RouteRecordRaw) {
+  const routes = [] as RouteRecordRaw[];
+  Object.entries(views).forEach(([file, module]) => {
+    if (file.includes(`../views/${layoutRoute.name as string}`)) {
+      const route = getRoutesByModule(file, module);
+      routes.push(route);
+    }
+  });
+  return routes;
 }
-/**
- * 
- * @param file 获取布局路由
- * @param module 
- * @returns 
- */
+
+//获取布局路由
 function getRoutesByModule(file: string, module: { [key: string]: any }) {
-    const name = file.split("/").pop()?.split('.')[0]
-    // console.log(file.match(/\.\.\/layouts\/(?<name>.+?)\.vue/i)?.groups?.name);
-    // const name = file.replace(/.+layouts\/ |.+views|\.vue/gi, '')
-    const route = {
-        path: `/${name}`,
-        component: module,
-        name: name,
-        meta: {
-            title: name
-        }
-    } as RouteRecordRaw
-    
-    return route
-
+  // const name = file.split("/").pop()?.split('.')[0]
+  // console.log(file.match(/\.\.\/layouts\/(?<name>.+?)\.vue/i)?.groups?.name);
+  const name = file.replace(/.+layouts\/|.+views\/|\.vue/gi, "");
+  const route = {
+    path: `/${name}`,
+    component: module,
+    name: name.replace("/", "."),
+  } as RouteRecordRaw;
+  return Object.assign(route, module.path?.path);
 }
-
 
 export default getRoutes();
--- src/views/admin/user.vue
+++ src/views/admin/user.vue
@@ -3,7 +3,12 @@
   <router-view />
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts"></script>
+<style scoped lang="scss"></style>
+
+<script>
+//自定义路由规则
+export default {
+  route: { path: "/user" },
+};
 </script>
-<style scoped lang='scss'>
-</style>
